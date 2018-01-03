eecbdfb5587cbd335babbf2d31e3638cb6a4a2c3
Florent <florent.vite@emagma.fr>
2018-01-03 19:41:32 +0100

    Add menu dropdown

--- static/js/app.js
+++ static/js/app.js
@@ -1,4 +1,9 @@
-$(document).foundation();
-jQuery(document).ready(function($){
-
+document.addEventListener("DOMContentLoaded", function(event) {
+    document.querySelector("#menu-dropdown").onclick = function() {
+        if(document.querySelector(".header__connexion").classList.contains("header__activated")) {
+            document.querySelector(".header__connexion").classList.remove("header__activated");
+        } else {
+            document.querySelector(".header__connexion").classList.add("header__activated");
+        }
+    };
 });
