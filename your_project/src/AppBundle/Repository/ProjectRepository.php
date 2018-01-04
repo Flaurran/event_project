1190bbc5c4897c17fff54ce51b250cbfa3eb9d01
Raphael Bader <raphael.bader@netapsys.fr>
2018-01-04 09:09:22 +0100

    FIXED: Exclusion des projets créés par l'utilisateur dans les listes public / privé

--- your_project/src/AppBundle/Repository/ProjectRepository.php
+++ your_project/src/AppBundle/Repository/ProjectRepository.php
@@ -2,6 +2,9 @@
 
 namespace AppBundle\Repository;
 
+use AppBundle\Entity\Project;
+use AppBundle\Entity\User;
+
 /**
  * ProjectRepository
  *
@@ -10,4 +13,58 @@ namespace AppBundle\Repository;
  */
 class ProjectRepository extends BaseRepository
 {
+
+    /**
+     * @param User|null $excludeUser
+     * @param array     $orderBy
+     *
+     * @return array
+     */
+    public function findPublicExcludeUser(User $excludeUser = null, $orderBy = [])
+    {
+        return $this->findProjectByContextAndExcludeUserQuery(Project::CONTEXT_PUBLIC, $excludeUser, $orderBy)->getQuery()->getResult();
+    }
+
+    /**
+     * @param User|null $excludeUser
+     * @param array     $orderBy
+     *
+     * @return array
+     */
+    public function findPrivateExcludeUser(User $excludeUser = null, $orderBy = [])
+    {
+        return $this->findProjectByContextAndExcludeUserQuery(Project::CONTEXT_PRIVATE, $excludeUser, $orderBy)->getQuery()->getResult();
+    }
+
+    /**
+     * @param      $context
+     * @param User $excludeUser
+     * @param      $orderBy
+     * @param      $alias
+     *
+     * @return \Doctrine\ORM\QueryBuilder
+     */
+    private function findProjectByContextAndExcludeUserQuery($context, User $excludeUser, $orderBy, $alias = 'p')
+    {
+        $qb = $this->createQueryBuilder($alias);
+        $qb
+            ->where(
+                $qb->expr()->eq($alias . '.context', ':context')
+            )
+            ->setParameter('context', $context)
+        ;
+        if ($excludeUser) {
+            $qb
+                ->andWhere(
+                    $qb->expr()->neq($alias . '.creator', ':user')
+                )
+                ->setParameter('user', $excludeUser->getId())
+            ;
+        }
+
+        foreach ($orderBy as $sort => $order) {
+            $qb->addOrderBy($alias . '.' . $sort, $order);
+        }
+        return $qb;
+    }
 }
